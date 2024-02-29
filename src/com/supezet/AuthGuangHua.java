bf2cd68de5778830287749de61cb243f02ad892c
Tanxiao <13671985248@163.com>
2024-02-29 16:32:45 +0800

    暂存培训文件单点登录部分

---
+++ src/com/supezet/AuthGuangHua.java
@@ -0,0 +1,23 @@
+package com.supezet;
+
+/**
+ * @Author: 张骏山
+ * @Date: 2024/2/29 11:27
+ * @PackageName: com.supezet
+ * @ClassName: AuthGuangHua
+ * @Description: 培训系统时代光华单点登录
+ * @Version: 1.0
+ **/
+public class AuthGuangHua {
+
+    private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    private static final String SECRET = "";
+
+    private static final String CORP_CODE = "";
+
+    public String getParam(String secret, String corpCode, String userName) {
+        return "";
+    }
+}
